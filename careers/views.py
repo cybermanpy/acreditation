129421c4c6a4e12b13ffb15a60ae2b6400cec898
Diego Saldivar <fhersa@gmail.com>
2016-08-10 14:45:20 -0400

    Paginator view Careers

--- careers/views.py
+++ careers/views.py
@@ -4,15 +4,35 @@ from django.http import HttpResponse
 #from django.shortcuts import render
 from django.template import loader
 from .models import Career
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 # Create your views here.
 
+# def viewCareers(request):
+#     title = 'Carreras Acreditadas Modelo Nacional'
+#     careers = Career.objects.all().order_by('national')
+#     template = loader.get_template('view_careers.html')
+#     context = {
+#         'careers': careers,
+#         'title':  title,
+#     }
+#     return HttpResponse(template.render(context, request))
+
 def viewCareers(request):
     title = 'Carreras Acreditadas Modelo Nacional'
-    careers = Career.objects.all().order_by('national')
     template = loader.get_template('view_careers.html')
+    listCareer = Career.objects.all().order_by('national')
+    paginator = Paginator(listCareer, 5)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+    try:
+        careers = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        careers = paginator.page(paginator.num_pages)
     context = {
         'careers': careers,
-        'title':  title,
+        'title': title,
     }
     return HttpResponse(template.render(context, request))
