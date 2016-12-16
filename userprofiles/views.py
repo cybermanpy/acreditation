7b44b8ca25ec13cf35c6a8dabfeb63a45d706084
Diego Saldivar <fhersa@gmail.com>
2016-12-16 14:31:09 -0300

    template login

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,33 +1,58 @@
 # coding=utf-8
 
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+# from django.shortcuts import render, redirect
+# from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.contrib.auth.decorators import login_required
+
 
 
 def authentication(request):
     title = 'Login'
     template = loader.get_template('login.html')
-    text = 'Login'
+    if not request.user.is_anonymous():
+        return HttpResponseRedirect('/dashboard')
     if request.method == 'POST':
-        action = request.POST.get('action', None)
-        username = request.POST.get('username', None)
-        passwd = request.POST.get('passwd', None)
-        user = authenticate(username=username, password=passwd)
-        if action == 'signup':
-            user = User.objects.create_user(username=username, password=passwd)
-            user.save()
-        elif action == 'login':
-            login(request, user)
-        return redirect('/dashboard')
+        form = AuthenticationForm(request.POST)
+        if form.is_valid:
+            user = request.POST['username']
+            passwd = request.POST['password']
+            access = authenticate(username=user, password=passwd)
+            if access is not None:
+                if access.is_active:
+                    login(request, access)
+                    return HttpResponseRedirect('/dashboard')
+                else:
+                    text = 'El usuario no esta Activo'
+                    contextNoActive = {
+                        'text': text,
+                        'form': form,
+                        'title': title,
+                    }
+                    return HttpResponse(template.render(contextNoActive, request))
+            else:
+                text = 'El usuario o la contraseña son incorrectos'
+                contextNoUser = {
+                    'text': text,
+                    'form': form,
+                    'title': title,
+                }
+                return HttpResponse(template.render(contextNoUser, request))
+
+    else:
+        form = AuthenticationForm()
+    text = ""
     context = {
         'title': title,
+        'form': form,
         'text': text,
     }
     return HttpResponse(template.render(context, request))
 
+@login_required(login_url='/login')
 def dashboard(request):
     title = 'Hello'
     template = loader.get_template('dashboard.html')
@@ -35,3 +60,25 @@ def dashboard(request):
         'title': title,
     }
     return HttpResponse(template.render(context, request))
+
+
+# def authentication(request):
+#     title = 'Login'
+#     template = loader.get_template('login.html')
+#     text = 'Login'
+#     if request.method == 'POST':
+#         action = request.POST.get('action', None)
+#         username = request.POST.get('username', None)
+#         passwd = request.POST.get('passwd', None)
+#         user = authenticate(username=username, password=passwd)
+#         if action == 'signup':
+#             user = User.objects.create_user(username=username, password=passwd)
+#             user.save()
+#         elif action == 'login':
+#             login(request, user)
+#         return redirect('/dashboard')
+#     context = {
+#         'title': title,
+#         'text': text,
+#     }
+#     return HttpResponse(template.render(context, request))
